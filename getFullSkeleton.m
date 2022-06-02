327bda26aea164e99d46ed11232b4560ea2586c3
Maciej Papciak <maciekpapciak@pm.me>
2022-06-02 23:23:52 +0200

    fixed pseudo img build

--- getFullSkeleton.m
+++ getFullSkeleton.m
@@ -1,16 +1,15 @@
 function [imWithPoses, bodyCoords, bodyParts] = getFullSkeleton(filename, xRadius, yRadius)
     im = imread(filename);
     [imWithPoses, bodyParts] = getOpenPoseSkeleton(im);
-    size(bodyParts, 1)
     if size(bodyParts,1) == 11
         handCoords = [bodyParts(4,3), bodyParts(4,4); bodyParts(6,3), bodyParts(6,4)];
-    elseif size(bodyParts, 1) == 9
+    else
         handCoords = [bodyParts(4,3), bodyParts(4,4)];
     end
     finalHand = [];
     for i=1:size(handCoords,1)
         handIm = imcrop(imWithPoses, [handCoords(i,1)- xRadius, handCoords(i,2)-yRadius, xRadius*2, yRadius]);
-        figure;imshow(handIm)
+%         figure;imshow(handIm)
         [skeleton] = getSkeleton(handIm);
         
         % recalculate hand skeleton coords
--- main.m
+++ main.m
@@ -1,4 +1,2 @@
-[img, bodyCoords, bodyParts] = getFullSkeleton('img/03.jpg', 70, 120);
-figure;imshow(img)
-% [pseudoImg] = buildPseudoImage('img', 70, 120);
+[pseudoImg] = buildPseudoImage('data/test', 70, 120);
 % imshow(pseudoImg)
